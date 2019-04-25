7c80afc4cc6bef01b8737bb48f95f0b9d699a347
xujimao <jimaoxu@163.com>
2019-04-25 16:57:25 +0800

    add xadmin

--- conan/settings.py
+++ conan/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0,os.path.join(BASE_DIR, 'extar_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'explore.apps.ExploreConfig',
     'common.apps.CommonConfig',
     'utils',
-    #'xadmin',
-    #'crispy_forms',
+    'xadmin',
+    'crispy_forms',
    # 'reversion',
 ]
 
