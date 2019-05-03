9bb50bdcd553d9a3fc6057a0cb75ee226c342a99
bamboo <chengzhengwu@aliyun.com>
2019-05-03 10:51:03 +0800

    'fix'

--- contest/urls.py
+++ contest/urls.py
@@ -11,15 +11,15 @@ urlpatterns = [
     path(r'previous/', views.ContestPreviousView.as_view()),
     #path(r'<path:contest_name>/detail/', views.ContestDetailView.as_view()),
     # path(r'virtual_contest/random/', views.VirtualRandomView.as_view()),
-    path(r'<path:contest_name>/', views.ContestView.as_view()),
-    #path(r'<path:contest_name>/detail/', views.ContestDetailView.as_view()),
-    # path(r'<path:contest_name>/problems/', views.ContestView.as_view()),
-    # path(r'<path:contest_name>/problems/<path: problem_name>/',
-    #      views.ContestProblemDetailView.as_view()),
-    # path(r'virtual_contest/<path:contest_name>/', views.VirtualContestView.as_view()),
-    # path(r'virtual_contest/<path:contest_name>/problems/',
-    #      views.VirtualContestProblemView.as_view()),
-    # path(r'virtual_contest/<path:contest_name>/problems/<path: problem_name>/',
-    #      views.VirtualContestProblemDetailView.as_view())
+    path(r'<str:contest_name>/', views.ContestView.as_view()),
+    path(r'<str:contest_name>/detail/', views.ContestDetailView.as_view()),
+    path(r'<str:contest_name>/problems/', views.ContestView.as_view()),
+    path(r'<str:contest_name>/problems/<str: problem_name>/',
+         views.ContestProblemDetailView.as_view()),
+    path(r'virtual_contest/<str:contest_name>/', views.VirtualContestView.as_view()),
+    path(r'virtual_contest/<str:contest_name>/problems/',
+         views.VirtualContestProblemView.as_view()),
+    path(r'virtual_contest/<str:contest_name>/problems/<str: problem_name>/',
+         views.VirtualContestProblemDetailView.as_view())
 ]
 
